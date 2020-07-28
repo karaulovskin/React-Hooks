d5495cf409f034a2aef7beb3cb85376fe9858579
karaulovskin <karaulovskin@yandex.ru>
2020-07-28 22:47:59 +0300

    Создание github state. Урок 2

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,5 +1,6 @@
 import React, {useReducer} from "react"
 import {githubReducer} from "./githubReducer";
+import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../types";
 
 export const GithubState = ({children}) => {
     const initialState = {
@@ -10,9 +11,39 @@ export const GithubState = ({children}) => {
     }
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
+    const search = async value => {
+        setLoading()
+        dispatch({
+            type: SEARCH_USERS,
+            payload: []
+        })
+    }
+
+    const getUser = async name => {
+        setLoading()
+        dispatch({
+            type: GET_USER,
+            payload: {}
+        })
+    }
+
+    const getRepos = async name => {
+        setLoading()
+        dispatch({
+            type: GET_REPOS,
+            payload: []
+        })
+    }
+
+    const clearUsers = () => dispatch({type: CLEAR_USERS})
+
+    const setLoading = () => dispatch({type: SET_LOADING})
+
+    const {user, users, repos, loading} = state
+
     return (
         <GithubState.Provider value={{
-
+            setLoading, search, getUser, getRepos, clearUsers
         }}>
             {children}
         </GithubState.Provider>
